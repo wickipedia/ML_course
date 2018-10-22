9ec24a127b054ab97c3676baf1c27eb599341ded
wickipedia <eliaswicki@bluemail.ch>
2018-10-22 16:46:51 +0200

    lab5

--- labs/ex05/template/least_squares.py
+++ labs/ex05/template/least_squares.py
@@ -8,10 +8,15 @@ import numpy as np
 
 
 def least_squares(y, tx):
-    """calculate the least squares."""
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # least squares: TODO
-    # returns mse, and optimal weights
-    # ***************************************************
-    raise NotImplementedError
+    '''
+    Least squares regression using normal equations
+    arguments:
+        y - output data, target
+        tx - input data, features
+    return:
+        w - weigth of last iteration
+        loss - mean square error of last iteration
+    '''
+    #return np.linalg.lstsq(np.transpose(tx).dot(tx), np.transpose(tx).dot(y),rcond=None)[0]
+    w=np.linalg.solve(np.transpose(tx).dot(tx), np.transpose(tx).dot(y))
+    return w
